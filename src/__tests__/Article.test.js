4f1f174cba02c616cf3eb903611bfc2549d99b82
Bianca Kioko <kiokobianca01@gmail.com>
2024-04-22 22:35:56 +0300

    uncommenteted content to pass test

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,18 @@ import React from "react";
 
 import Article from "../components/Article";
 
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+
+//   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+// });
+
+// src/__tests__/Article.test.js
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+
+  // add this line
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
